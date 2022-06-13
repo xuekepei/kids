c8eb703a3875b2d0cdf08418e84246f5d7a9ef79
xueke <xuekepei@gmail.com>
2022-06-13 22:30:17 +0900

    :art: 删除 vite

--- src/api/module/auth.js
+++ src/api/module/auth.js
@@ -1,26 +1,26 @@
-const kidsBaseUrl = import.meta.env.VITE_API_BASE_URL+'/kids'
-const dicBaseUrl = import.meta.env.VITE_API_BASE_URL+'/dic'
-const authBaseUrl = import.meta.env.VITE_API_BASE_URL+'/account'
+const kidsBaseUrl = process.env.VUE_APP_API_BASE_URL +'/kids'
+const dicBaseUrl = process.env.VUE_APP_API_BASE_URL +'/dic'
+const authBaseUrl = process.env.VUE_APP_API_BASE_URL+'/account'
 
 export default function (api) {
     return {
         login(data) {
-            return api.post(authBaseUrl + "/login", data)
+            return api.post(authBaseUrl + '/login', data)
         },
         letters() {
-            return api.get(kidsBaseUrl + "/letters")
+            return api.get(kidsBaseUrl + '/letters')
         },
         search(word) {
-            return api.get(dicBaseUrl + "/search?word=" + word);
+            return api.get(dicBaseUrl + '/search?word=' + word);
         },
         letterWords(letter) {
-            return api.get(kidsBaseUrl + "/letters/" + letter + "/words")
+            return api.get(kidsBaseUrl + '/letters/' + letter + '/words')
         },
         allWords() {
-            return api.get(kidsBaseUrl + "/words")
+            return api.get(kidsBaseUrl + '/words')
         },
         add(letter, data) {
-            return api.post(kidsBaseUrl + "/letters/" + letter + "/words", data, { needToken: true })
+            return api.post(kidsBaseUrl + '/letters/' + letter + '/words', data, { needToken: true })
         },
     }
 }
